a001939493303f4190e25f6548a51662b8c35255
Michl Steglich <mrgreaka@gmail.com>
2019-07-16 13:34:35 +0200

    prevent dataraces in the builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate lazy_static;
 use std::{
     ffi::{CStr, CString},
     os::raw::c_char,
     ptr::null,
+    sync::RwLock,
 };
 use winapi::shared::{
     minwindef::{LPARAM, LPVOID, UINT, WPARAM},
@@ -9,8 +12,18 @@ use winapi::shared::{
     windef::HWND,
 };
 
-static mut FUNCTIONS: Option<ArcdpsFunctions> = None;
-static mut INFO: Option<(CString, CString)> = None;
+lazy_static! {
+    static ref FUNCTIONS: RwLock<ArcdpsFunctions> = {
+        RwLock::new(ArcdpsFunctions {
+            combat: None,
+            imgui: None,
+            options_end: None,
+            combat_local: None,
+            options_windows: None,
+        })
+    };
+    static ref INFO: RwLock<Option<(CString, CString)>> = { RwLock::new(None) };
+}
 
 pub type WndprocCallback = fn(hWnd: HWND, uMsg: UINT, wParam: WPARAM, lParam: LPARAM) -> usize;
 /*
@@ -50,31 +63,21 @@ struct ArcdpsFunctions {
     pub options_windows: Option<SafeOptionsWindowsCallback>,
 }
 
-fn options_wrapper() -> usize {
-    let func = unsafe {
-        if let Some(funcs) = &FUNCTIONS {
-            funcs.options_end.unwrap()
-        } else {
-            return 0;
-        }
-    };
+unsafe fn options_wrapper() -> usize {
+    let funcs = FUNCTIONS.read().unwrap();
+    let func = funcs.options_end.unwrap();
     func();
     0
 }
 
-fn options_windows_wrapper(windowname: PCCHAR) -> usize {
-    let func = unsafe {
-        if let Some(funcs) = &FUNCTIONS {
-            funcs.options_windows.unwrap()
-        } else {
-            return 0;
-        }
-    };
+unsafe fn options_windows_wrapper(windowname: PCCHAR) -> usize {
+    let funcs = FUNCTIONS.read().unwrap();
+    let func = funcs.options_windows.unwrap();
     func(get_str_from_pcchar(windowname));
     0
 }
 
-fn cbt_wrapper_area(
+unsafe fn cbt_wrapper_area(
     ev: *mut cbtevent,
     src: *mut u_ag,
     dst: *mut u_ag,
@@ -82,17 +85,12 @@ fn cbt_wrapper_area(
     id: u64,
     revision: u64,
 ) -> usize {
-    let func = unsafe {
-        if let Some(funcs) = &FUNCTIONS {
-            funcs.combat.unwrap()
-        } else {
-            return 0;
-        }
-    };
+    let funcs = FUNCTIONS.read().unwrap();
+    let func = funcs.combat.unwrap();
     cbt_wrapper(func, ev, src, dst, skillname, id, revision)
 }
 
-fn cbt_wrapper_local(
+unsafe fn cbt_wrapper_local(
     ev: *mut cbtevent,
     src: *mut u_ag,
     dst: *mut u_ag,
@@ -100,17 +98,12 @@ fn cbt_wrapper_local(
     id: u64,
     revision: u64,
 ) -> usize {
-    let func = unsafe {
-        if let Some(funcs) = &FUNCTIONS {
-            funcs.combat_local.unwrap()
-        } else {
-            return 0;
-        }
-    };
+    let funcs = FUNCTIONS.read().unwrap();
+    let func = funcs.combat_local.unwrap();
     cbt_wrapper(func, ev, src, dst, skillname, id, revision)
 }
 
-fn cbt_wrapper(
+unsafe fn cbt_wrapper(
     func: SafeCombatCallback,
     ev: *mut cbtevent,
     src: *mut u_ag,
@@ -125,40 +118,34 @@ fn cbt_wrapper(
     let s_skillname: Option<&str>;
     let p_src;
     let p_dst;
-    unsafe {
-        s_ev = if ev.is_null() { None } else { Some(&*ev) };
-        s_src = if src.is_null() {
-            None
-        } else {
-            p_src = get_safe_ag(&*src);
-            Some(&p_src)
-        };
-        s_dst = if dst.is_null() {
-            None
-        } else {
-            p_dst = get_safe_ag(&*dst);
-            Some(&p_dst)
-        };
-        s_skillname = get_str_from_pcchar(skillname);
-        func
+
+    s_ev = if ev.is_null() { None } else { Some(&*ev) };
+    s_src = if src.is_null() {
+        None
+    } else {
+        p_src = get_safe_ag(&*src);
+        Some(&p_src)
     };
+    s_dst = if dst.is_null() {
+        None
+    } else {
+        p_dst = get_safe_ag(&*dst);
+        Some(&p_dst)
+    };
+    s_skillname = get_str_from_pcchar(skillname);
+
     func(s_ev, s_src, s_dst, s_skillname, id, revision);
     0
 }
 
-fn imgui_wrapper(not_charsel_or_loading: u32) -> usize {
-    let func = unsafe {
-        if let Some(funcs) = &FUNCTIONS {
-            funcs.imgui.unwrap()
-        } else {
-            return 0;
-        }
-    };
+unsafe fn imgui_wrapper(not_charsel_or_loading: u32) -> usize {
+    let funcs = FUNCTIONS.read().unwrap();
+    let func = funcs.imgui.unwrap();
     func(not_charsel_or_loading != 0);
     0
 }
 
-fn get_safe_ag(ag: &u_ag) -> Ag {
+unsafe fn get_safe_ag(ag: &u_ag) -> Ag {
     let name = get_str_from_pcchar(ag.name);
     Ag {
         name,
@@ -170,40 +157,34 @@ fn get_safe_ag(ag: &u_ag) -> Ag {
     }
 }
 
-fn get_str_from_pcchar<'a>(src: PCCHAR) -> Option<&'a str> {
+unsafe fn get_str_from_pcchar<'a>(src: PCCHAR) -> Option<&'a str> {
     if src.is_null() {
         None
     } else {
-        Some(unsafe {
+        Some(
             CStr::from_ptr(src as *const c_char)
                 .to_str()
-                .unwrap_or_default()
-        })
+                .unwrap_or_default(),
+        )
     }
 }
 
 impl arcdps_exports {
     pub fn new(sig: usize, name: &'static str, build: &'static str) -> arcdps_exports {
-        let (name, build) = unsafe {
-            FUNCTIONS = Some(ArcdpsFunctions {
-                combat: None,
-                imgui: None,
-                options_end: None,
-                combat_local: None,
-                options_windows: None,
-            });
-            INFO = Some((CString::new(name).unwrap(), CString::new(build).unwrap()));
-            if let Some(infos) = &INFO {
-                infos
+        let (out_name, out_build) = {
+            let mut info = INFO.write().unwrap();
+            *info = Some((CString::new(name).unwrap(), CString::new(build).unwrap()));
+            if let Some((name, build)) = &*info {
+                (name.as_ptr() as PCCHAR, build.as_ptr() as PCCHAR)
             } else {
-                unreachable!();
+                unreachable!()
             }
         };
         arcdps_exports {
             size: std::mem::size_of::<arcdps_exports>(),
             sig,
-            out_name: name.as_ptr() as PCCHAR,
-            out_build: build.as_ptr() as PCCHAR,
+            out_name,
+            out_build,
             wnd_nofilter: null::<isize>() as LPVOID,
             combat: null::<isize>() as LPVOID,
             imgui: null::<isize>() as LPVOID,
@@ -221,41 +202,29 @@ impl arcdps_exports {
 
     pub fn combat(mut self, func: SafeCombatCallback) -> Self {
         self.combat = cbt_wrapper_area as LPVOID;
-        unsafe {
-            if let Some(funcs) = &mut FUNCTIONS {
-                funcs.combat = Some(func);
-            };
-        }
+        let mut funcs = FUNCTIONS.write().unwrap();
+        funcs.combat = Some(func);
         self
     }
 
     pub fn imgui(mut self, func: SafeImguiCallback) -> Self {
         self.imgui = imgui_wrapper as LPVOID;
-        unsafe {
-            if let Some(funcs) = &mut FUNCTIONS {
-                funcs.imgui = Some(func);
-            };
-        }
+        let mut funcs = FUNCTIONS.write().unwrap();
+        funcs.imgui = Some(func);
         self
     }
 
     pub fn options_end(mut self, func: SafeOptionsCallback) -> Self {
         self.options_end = options_wrapper as LPVOID;
-        unsafe {
-            if let Some(funcs) = &mut FUNCTIONS {
-                funcs.options_end = Some(func);
-            };
-        }
+        let mut funcs = FUNCTIONS.write().unwrap();
+        funcs.options_end = Some(func);
         self
     }
 
     pub fn combat_local(mut self, func: SafeCombatCallback) -> Self {
         self.combat_local = cbt_wrapper_local as LPVOID;
-        unsafe {
-            if let Some(funcs) = &mut FUNCTIONS {
-                funcs.combat_local = Some(func);
-            };
-        }
+        let mut funcs = FUNCTIONS.write().unwrap();
+        funcs.combat_local = Some(func);
         self
     }
 
@@ -266,11 +235,8 @@ impl arcdps_exports {
 
     pub fn options_windows(mut self, func: SafeOptionsWindowsCallback) -> Self {
         self.options_windows = options_windows_wrapper as LPVOID;
-        unsafe {
-            if let Some(funcs) = &mut FUNCTIONS {
-                funcs.options_windows = Some(func);
-            };
-        }
+        let mut funcs = FUNCTIONS.write().unwrap();
+        funcs.options_windows = Some(func);
         self
     }
 }
