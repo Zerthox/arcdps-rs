6d9c07a57d849017f93455ca54c0e9d9704543fc
Mirclus <Mirclus@users.noreply.github.com>
2021-07-14 23:46:25 +0200

    Remove warning without log feature

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -20,6 +20,8 @@ pub unsafe fn __init(arcdps: raw_structs::HANDLE, name: &'static str) {
     #[cfg(feature = "log")]
     let _ = log::set_boxed_logger(Box::new(logging::ArcdpsLogger::new(name)))
         .map(|()| log::set_max_level(log::LevelFilter::Trace));
+    #[cfg(not(feature="log"))]
+    let _ = name;   // only used for logging
 }
 
 /// This struct isn't used anywhere. It is a reference on what fields are
