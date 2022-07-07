c78a078cef5c02d93c5bd57e53aec9c812d4d8e7
Zerthox <zerthox@gmail.com>
2022-07-07 02:38:38 +0200

    Add enums to combat event

--- core/src/api/event.rs
+++ core/src/api/event.rs
@@ -1,6 +1,76 @@
+use super::{Activation, BuffRemove, StateChange, Team};
+
+/// ArcDPS Combat event.
 #[derive(Debug, Clone)]
 #[repr(C)]
 pub struct CombatEvent {
+    pub time: u64,
+    pub src_agent: usize,
+    pub dst_agent: usize,
+    pub value: i32,
+    pub buff_dmg: i32,
+    pub overstack_value: u32,
+    pub skill_id: u32,
+    pub src_instance_id: u16,
+    pub dst_instance_id: u16,
+    pub src_master_instance_id: u16,
+    pub dst_master_instance_id: u16,
+    pub iff: Team,
+    pub buff: u8,
+    pub result: u8,
+    pub is_activation: Activation,
+    pub is_buff_remove: BuffRemove,
+    pub is_ninety: u8,
+    pub is_fifty: u8,
+    pub is_moving: u8,
+    pub is_statechange: StateChange,
+    pub is_flanking: u8,
+    pub is_shields: u8,
+    pub is_off_cycle: u8,
+    pub pad61: u8,
+    pub pad62: u8,
+    pub pad63: u8,
+    pub pad64: u8,
+}
+
+impl From<&RawCombatEvent> for CombatEvent {
+    fn from(event: &RawCombatEvent) -> Self {
+        Self {
+            time: event.time,
+            src_agent: event.src_agent,
+            dst_agent: event.dst_agent,
+            value: event.value,
+            buff_dmg: event.buff_dmg,
+            overstack_value: event.overstack_value,
+            skill_id: event.skill_id,
+            src_instance_id: event.src_instance_id,
+            dst_instance_id: event.dst_instance_id,
+            src_master_instance_id: event.src_master_instance_id,
+            dst_master_instance_id: event.dst_master_instance_id,
+            iff: event.iff.into(),
+            buff: event.buff,
+            result: event.result,
+            is_activation: event.is_activation.into(),
+            is_buff_remove: event.is_buff_remove.into(),
+            is_ninety: event.is_ninety,
+            is_fifty: event.is_fifty,
+            is_moving: event.is_moving,
+            is_statechange: event.is_statechange.into(),
+            is_flanking: event.is_flanking,
+            is_shields: event.is_shields,
+            is_off_cycle: event.is_off_cycle,
+            pad61: event.pad61,
+            pad62: event.pad62,
+            pad63: event.pad63,
+            pad64: event.pad64,
+        }
+    }
+}
+
+/// Raw ArcDPS Combat event.
+#[derive(Debug, Clone)]
+#[repr(C)]
+pub struct RawCombatEvent {
     pub time: u64,
     pub src_agent: usize,
     pub dst_agent: usize,
--- core/src/util.rs
+++ core/src/util.rs
@@ -1,12 +1,12 @@
 use crate::{
-    api::{Agent, CombatEvent, RawAgent},
+    api::{Agent, CombatEvent, RawAgent, RawCombatEvent},
     extras::{RawUserInfo, UserInfo},
 };
 use std::{ffi::CStr, os::raw::c_char};
 
 // TODO: can we move any of this to somewhere better?
 
-/// A helper function to convert arcdps strings to [`&str`].
+/// Helper to convert arcdps strings to [`&str`].
 ///
 /// ### Remarks
 /// The result is not necessarily static.
@@ -23,29 +23,29 @@ pub unsafe fn get_str_from_pc_char(src: *const c_char) -> Option<&'static str> {
 }
 
 pub struct CombatEventArgs<'a> {
-    pub ev: Option<&'a CombatEvent>,
+    pub ev: Option<CombatEvent>,
     pub src: Option<Agent<'a>>,
     pub dst: Option<Agent<'a>>,
     pub skill_name: Option<&'static str>,
 }
 
-/// A helper function to convert raw arguments to safe abstractions
+/// Helper to convert raw arguments to safe abstractions
 #[inline(always)]
 pub fn get_combat_args_from_raw<'a>(
-    raw_ev: Option<&'a CombatEvent>,
+    raw_ev: Option<&'a RawCombatEvent>,
     raw_src: Option<&'a RawAgent>,
     raw_dst: Option<&'a RawAgent>,
     raw_skill_name: *const c_char,
 ) -> CombatEventArgs<'a> {
     CombatEventArgs {
-        ev: raw_ev,
+        ev: raw_ev.map(Into::into),
         src: raw_src.map(Into::into),
         dst: raw_dst.map(Into::into),
         skill_name: unsafe { get_str_from_pc_char(raw_skill_name) },
     }
 }
 
-/// A helper function to convert raw arguments to safe abstractions
+/// Helper to convert raw arguments to safe abstractions
 #[inline(always)]
 pub fn convert_extras_user(user: &RawUserInfo) -> UserInfo {
     let name = unsafe { get_str_from_pc_char(user.account_name as _) };
