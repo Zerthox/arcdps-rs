a8eed21f8f567f34ab7f0d1d25ac293930ccf3d1
Zerthox <zerthox@gmail.com>
2023-01-03 21:35:11 +0100

    Add example plugin crate

---
+++ example_plugin/src/lib.rs
@@ -0,0 +1,58 @@
+use arcdps::{
+    extras::{ExtrasAddonInfo, UserInfoIter, UserRole},
+    Agent, CombatEvent, StateChange,
+};
+use log::info;
+use std::error::Error;
+
+arcdps::export! {
+    name: "Example Plugin",
+    sig: 0x12345678, // change this to a random number
+    init,
+    combat,
+    extras_init,
+    extras_squad_update,
+}
+
+fn init() -> Result<(), Box<dyn Error>> {
+    info!("plugin has been started");
+    Ok(())
+}
+
+fn combat(
+    event: Option<CombatEvent>,
+    src: Option<Agent>,
+    _dst: Option<Agent>,
+    _skill_name: Option<&str>,
+    _id: u64,
+    _revision: u64,
+) {
+    if let (Some(event), Some(src)) = (event, src) {
+        if let StateChange::EnterCombat = event.is_statechange {
+            info!(
+                "{} ({}) has entered combat",
+                src.name.unwrap_or("unknown agent"),
+                src.id
+            );
+        }
+    }
+}
+
+fn extras_init(extras_info: ExtrasAddonInfo, account_name: Option<&str>) {
+    info!(
+        "extras version {} on account {}",
+        extras_info.string_version.unwrap_or("unknown"),
+        account_name.unwrap_or("unknown")
+    );
+}
+
+fn extras_squad_update(users: UserInfoIter) {
+    for user in users {
+        if let UserRole::SquadLeader | UserRole::Lieutenant = user.role {
+            info!(
+                "{} can place markers",
+                user.account_name.unwrap_or("unknown user")
+            );
+        }
+    }
+}
