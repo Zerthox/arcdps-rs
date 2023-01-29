a906eb732e66d64bee2ea1c6052d6b9fabdf3ec1
Zerthox <zerthox@gmail.com>
2023-01-29 20:39:24 +0100

    Fix clippy warning

--- arcdps/src/panic.rs
+++ arcdps/src/panic.rs
@@ -7,7 +7,7 @@ use std::panic;
 // TODO: rust will abort when reaching the ffi boundary, which skips arcs crash log
 pub fn init_panic_hook(name: &'static str) {
     panic::set_hook(Box::new(move |info| {
-        let msg = format!("{} {}", name, info);
+        let msg = format!("{name} {info}");
 
         // log the error to the arc log
         let _ = log_to_file(format!("error: {}", &msg));
