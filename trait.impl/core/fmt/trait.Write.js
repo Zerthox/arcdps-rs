55dfbc75d3d4827a291866ac53ed1515def324be
Zerthox <Zerthox@users.noreply.github.com>
2025-07-14 18:39:10 +0000

    deploy: d02c70d2a1013c6413665e28219c20b1b0257104

--- trait.impl/core/fmt/trait.Write.js
+++ trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arcdps_imgui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImString.html\" title=\"struct arcdps_imgui::ImString\">ImString</a>"]]]]);
+    var implementors = Object.fromEntries([["arcdps_imgui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImString.html\" title=\"struct arcdps_imgui::ImString\">ImString</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/arith/trait.MulAssign.js
+++ trait.impl/core/ops/arith/trait.MulAssign.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["evtc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.MulAssign.html\" title=\"trait core::ops::arith::MulAssign\">MulAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"evtc/position/struct.Position.html\" title=\"struct evtc::position::Position\">Position</a>"]]]]);
+    var implementors = Object.fromEntries([["evtc",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/ops/arith/trait.MulAssign.html\" title=\"trait core::ops::arith::MulAssign\">MulAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"evtc/position/struct.Position.html\" title=\"struct evtc::position::Position\">Position</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
