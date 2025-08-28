c0faa468e92f70a3535beb5a21e05fd5d3296530
Zerthox <Zerthox@users.noreply.github.com>
2025-08-28 15:43:08 +0000

    deploy: ee16ad96ad911fbad065602f1f1b5f480e1867c9

--- trait.impl/alloc/borrow/trait.ToOwned.js
+++ trait.impl/alloc/borrow/trait.ToOwned.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arcdps_imgui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImStr.html\" title=\"struct arcdps_imgui::ImStr\">ImStr</a>"]]]]);
+    var implementors = Object.fromEntries([["arcdps_imgui",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImStr.html\" title=\"struct arcdps_imgui::ImStr\">ImStr</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/marker/trait.Sized.js
+++ trait.impl/core/marker/trait.Sized.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arcdps_imgui",[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImStr.html\" title=\"struct arcdps_imgui::ImStr\">ImStr</a>",1,["arcdps_imgui::string::ImStr"]]]]]);
+    var implementors = Object.fromEntries([["arcdps_imgui",[["impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"arcdps_imgui/struct.ImStr.html\" title=\"struct arcdps_imgui::ImStr\">ImStr</a>",1,["arcdps_imgui::string::ImStr"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
