adbc6d61b79dafffec02a94de367fc579331ed08
Zerthox <Zerthox@users.noreply.github.com>
2025-10-13 18:28:57 +0000

    deploy: 8bb0657abd2463fef5b305ff4dd72ba04b535fd7

--- trait.impl/log/trait.Log.js
+++ trait.impl/log/trait.Log.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["arcdps",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.27/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.ArcDpsLogger.html\" title=\"struct arcdps::log::ArcDpsLogger\">ArcDpsLogger</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.27/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.FileLogger.html\" title=\"struct arcdps::log::FileLogger\">FileLogger</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.27/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.WindowLogger.html\" title=\"struct arcdps::log::WindowLogger\">WindowLogger</a>"]]],["log",[]]]);
+    var implementors = Object.fromEntries([["arcdps",[["impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.ArcDpsLogger.html\" title=\"struct arcdps::log::ArcDpsLogger\">ArcDpsLogger</a>"],["impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.FileLogger.html\" title=\"struct arcdps::log::FileLogger\">FileLogger</a>"],["impl <a class=\"trait\" href=\"log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"arcdps/log/struct.WindowLogger.html\" title=\"struct arcdps::log::WindowLogger\">WindowLogger</a>"]]],["log",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[741,11]}
+//{"start":57,"fragment_lengths":[660,11]}
